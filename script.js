e606d104a3ef7ae7c99cd1ea34fed71da483eeb0
Edward Miller <edmiller.me@gmail.com>
2020-10-21 20:08:23 +1300

    Completed FizzBuzz

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* LOOPING A TRIANGLE */
 var loopingATriangle = document.getElementById('LoopingTriangle');
 
 const makeTriangle = () => {
@@ -13,4 +14,42 @@ const makeTriangle = () => {
 
 const resetTriangle = () => {
     loopingATriangle.innerHTML = '';
+}
+
+for(var i = 1; i < 20; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(i + ' FizzBuzz');
+    }
+    else if(i % 5 === 0) {
+            console.log(i +' Buzz')
+    }
+    else if(i % 3 === 0) {
+            console.log(i +' Fizz');
+    }
+    else {
+        console.log(i);
+    }
+} 
+/* FIZZ BUZZ */
+var fizzBuzz = document.getElementById('fizzBuzz');
+
+const FizzBuzz = () => {
+    for(var i = 1; i < 12; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            fizzBuzz.innerHTML += 'FizzBuzz' + '<br/>';
+        }
+        else if(i % 5 === 0) {
+                fizzBuzz.innerHTML += 'Buzz' + '<br/>';
+        }
+        else if(i % 3 === 0) {
+                fizzBuzz.innerHTML += 'Fizz' + '<br/>';
+        }
+        else {
+            fizzBuzz.innerHTML += i + '<br/>';
+        }
+    }
+}
+
+const resetFizzBuzz = () => {
+    fizzBuzz.innerHTML = '';
 }
